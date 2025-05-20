f3b31495a8c14200fa0efa37f3cbde70f1b5d0c8
adimachmali <adimachmali@gmail.com>
2025-05-21 01:09:54 +0300

    github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
-import { GlobalStyle } from "./styles/globalStyles";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import { GlobalStyle } from './styles/globalStyles';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Router>
+      <Router basename="/movie-catalog-app/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
